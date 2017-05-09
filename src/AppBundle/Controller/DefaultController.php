d69eacb1febd8f2dde5404d748cfb55df6ba4fc2
hidabe <fernando@sopinet.com>
2017-05-09 22:10:39 +0200

    added landing

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,6 +14,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        return $this->redirect($this->generateUrl('panel_dashboard'));
+        return $this->render('::public/landing.html.twig');
+        //return $this->redirect($this->generateUrl('panel_dashboard'));
     }
 }
